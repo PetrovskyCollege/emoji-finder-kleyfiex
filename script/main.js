e819236f831c42fc1d6ab2396e3ce037ffa6e809
kleyfiex <kleyfie92@gmail.com>
2023-10-28 22:27:45 +0300

    Фикс багов

--- script/main.js
+++ script/main.js
@@ -5,15 +5,15 @@ const emojiContainer = document.getElementById('cards'); //получаем сс
 
 data.forEach(emoji => {
     const block = document.createElement('div'); //создаем див
-    block.classList.add('block');
+    block.classList.add('block'); // добавляем класс к диву
 
     const symbolSpan = document.createElement('span');
-    symbolSpan.textContent = emoji.symbol;
+    symbolSpan.textContent = emoji.symbol; // записываем символ эмоджи в текст карточки
     block.appendChild(symbolSpan); // добавляем элемент в HTML
 
     const titleSpan = document.createElement('span');
-    titleSpan.textContent = emoji.title;
-    block.appendChild(titleSpan);
+    titleSpan.textContent = emoji.title; // записываем заголовок эмоджи в заголовок карточки
+    block.appendChild(titleSpan);   
 
     const keywordsP = document.createElement('p');
     keywordsP.textContent = emoji.keywords;
@@ -27,11 +27,11 @@ data.forEach(emoji => {
 const keyInput = document.getElementById('key'); // ссылка на инпут
 
 function emojiSort() {
-    const userKeyword = keyInput.value.toLowerCase(); // получение введенного от пользователя + нижний регистр
+    const userKeyword = keyInput.value.toLowerCase(); // получение введенного от пользователя слова в нижний регистр
     const blocksEmoji = emojiContainer.querySelectorAll('.block'); //берем все блоки
-
+    // проходимся по всем карточкам
     blocksEmoji.forEach(block => {
-        const keyword = block.querySelector('p').textContent.toLowerCase(); //получение коюч слов и нижний регистр
+        const keyword = block.querySelector('p').textContent.toLowerCase(); //конверт в нижний регистр
 
         if (keyword.includes(userKeyword)) {
             block.style.display = 'flex'; // отображаем блок
